57a50db053598609bafcf4784d4a6f41387528cb
Adair <adairmel9808@gmail.com>
2026-09-14 16:32:21 -0400

    Created weekly goal function and form

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ This project is a web-based dashboard built for WEB-115 to demonstrate interacti
 - [ ] Add color and style 
 - [ ] Integrate a list of tasks.
 - [ ] Add JavaScript logic for today's date.
+- [X] Add a weekly task goal calculator
+
+## Weekly Task Goals:
+This feature calculates a user's total task target for the week based on their daily goals 
+and additional bonus tasks. By capturing user input through a web form and processing the data 
+with a JavaScript function, the dashboard computes and displays both the base 
+5 day workload and the total weekly target directly on the page.
---
+++ js/task-manager.js
@@ -0,0 +1,27 @@
+function weeklyGoal(userName, dailyGoal, bonusTasks) {
+
+    // calculations
+    const weeklyGoalCount = Number(dailyGoal) * 5;
+    const totalGoal = weeklyGoalCount + Number(bonusTasks);
+
+    //assign output to variable
+    const output = "User:" + userName + "<br>" + "Total Weekly Goal: " + totalGoal;
+    
+    //update innerHTML
+    document.getElementById("goal-message").innerHTML = output;
+
+}   
+// add click event listener to "goal-btn"
+document.getElementById("goal-btn").addEventListener("click", function(event) {
+    
+    // Prevent form submission
+    event.preventDefault();
+
+    // retrieve input values from form
+    const userName = document.getElementById("userName").value;
+    const dailyGoal = document.getElementById("dailyGoal").value;
+    const bonusTasks = document.getElementById("bonusTasks").value;
+
+    // call weejkyGoal with form values
+    weeklyGoal(userName, dailyGoal, bonusTasks);
+});
